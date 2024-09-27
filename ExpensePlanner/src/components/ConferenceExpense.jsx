61ba15a616c81f3be1b42cc570c813040d5bd092
WFriver <wrivera@quickapps.mx>
2024-09-27 10:26:21 -0500

    Imagenes
    Se creo archivo de imagenes con su importacion

--- ExpensePlanner/src/components/ConferenceExpense.jsx
+++ ExpensePlanner/src/components/ConferenceExpense.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar'; 
-import ConferenceRoomImage from './Imagenes/ConferneceRoom.jpg';
+import images from './Imagenes/images';
 // import './ConferneceExpense.css';
 
 const rooms = [
-    { id: 1, name: "Conference Room", capacity: 15, price: 1500, image: ConfernenceRoomImage.jpg},
-    { id: 2, name: "Auditorium Hall", capacity: 200, price: 5500, image: "/path-to-auditorium-hall-image.jpg" },
-    { id: 3, name: "Presentation Room", capacity: 50, price: 3500, image: "/path-to-presentation-room-image.jpg" },
-    { id: 4, name: "Large Meeting Room", capacity: 10, price: 1000, image: "/path-to-large-meeting-room-image.jpg" },
-    { id: 5, name: "Small Meeting Room", capacity: 5, price: 800, image: "/path-to-small-meeting-room-image.jpg" },
+    { id: 1, name: "Conference Room", capacity: 15, price: 1500, image: images.ConferenceRoomImage},
+    { id: 2, name: "Auditorium Hall", capacity: 200, price: 5500, image: images.AuditoriumHallImage },
+    { id: 3, name: "Presentation Room", capacity: 50, price: 3500, image: images.PresentationRoomImage },
+    { id: 4, name: "Large Meeting Room", capacity: 10, price: 1000, image: images.LargeMeetingRoomImage },
+    { id: 5, name: "Small Meeting Room", capacity: 5, price: 800, image: images.SmallMeetingRoomImage },
   ];
 
 const ConferenceExpense = () =>{
