acb8a6e92dad0fb2b1a45c3cae00fbdbca9a93cf
WFriver <wrivera@quickapps.mx>
2024-10-03 14:55:05 -0500

    ecommerce
    cambios pero no se visualisa la salida ver que esta sucediendo

--- ecommerce/ecommerce/src/Components/ProductsList.jsx
+++ ecommerce/ecommerce/src/Components/ProductsList.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "../Components/ProductsList.css";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addItemToCart } from "./CartSlice";
 const ProuctList = () => {
+    const dispatch = useDispatch();
+    const [disabledProducts, setDiableproducts] = useState([]);
+
+    const handleAddToCart = product => {
+        dispatch(addItemToCart(product));
+        setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+      };
+
+
     const products = [
         { id: 1, name: 'Product A', price: 60 },
         { id: 2, name: 'Product B', price: 75 },
@@ -8,21 +20,21 @@ const ProuctList = () => {
     ];
 
     return (
-       <>
-       <div className="principal">
-        <div className="h2">
-            <h2>Products</h2>
-        </div>
-        <div className="row">
-            <div className="text">
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-             <div className="bu">
-                <button>hola</button>
-             </div>
-        </div>
-       </div>
-       </>
+        <div className="product-list">
+        <h2 className="product-list-title">Products</h2>
+        <ul className="product-list-items">
+            {products.map(product => (
+                <li key={product.id} className="product-list-item">
+                    <span>{product.name} - ${product.price}</span>
+                    <button className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                    onClick={() => handleAddToCart(product)}
+                    disabled={disabledProducts.includes(product.id)} >
+                        Add to Cart
+                    </button>
+                </li>
+            ))}
+        </ul>
+      </div>
     )
 }
 
